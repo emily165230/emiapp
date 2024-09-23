b1ce0b2ab21051d247a93a5a50a4baf44676f05c
emily165230 <emilyr28110@gmail.com>
2024-09-23 15:19:01 +0300

    first try

--- app/src/main/java/com/example/emiapp/MainActivity.java
+++ app/src/main/java/com/example/emiapp/MainActivity.java
@@ -1,24 +1,44 @@
 package com.example.emiapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        counter = 10;
+
+
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(String.valueOf(counter));
+    }
+
+    public void knock(View view) {
+        if (counter > 0) {
+            counter--;
+            TextView counterTextView = findViewById(R.id.textview_counter);
+            counterTextView.setText(String.valueOf(counter));
+
+            if (counter == 0) {
+                ImageView eggImageView = findViewById(R.id.imageview_egg);
+                eggImageView.setImageResource(R.drawable.suprise);
+            }
+        }
+    }
+
+    public void reset(View view) {
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(String.valueOf(counter));
+        ImageView eggImageView = findViewById(R.id.imageview_egg);
+        eggImageView.setImageResource(R.drawable.green);
     }
 }
